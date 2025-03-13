f60da45193712059551dd43858e4b6ab4aa691b2
Carlos Júnior <carlosbuanaoda@gmail.com>
2025-03-13 14:04:05 +0000

    add prettier and scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto de estudos que tem por objetivo clonar o https://www.tabnews.com.br/
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-  return <h1>"Tenhamos confiança ilimitada na Imaculada." São Maximiliano Kolbe</h1>
+  return (
+    <h1>"Tenhamos confiança ilimitada na Imaculada." São Maximiliano Kolbe</h1>
+  );
 }
 
 export default Home;
